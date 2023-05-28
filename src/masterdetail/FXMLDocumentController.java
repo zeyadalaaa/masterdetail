3244da08b3bdefff84034d45ef6eefaa802d6fc2
zeyadalaaa <zeyad.alaaeldin2021@gmail.com>
2023-05-28 17:14:28 +0300

    Update Record

--- src/masterdetail/FXMLDocumentController.java
+++ src/masterdetail/FXMLDocumentController.java
@@ -48,7 +48,7 @@ public class FXMLDocumentController implements Initializable {
         
         String firstName = firstNameText.getText();
         String lastName = lastNameText.getText();
-        String age = ageText.getText(); //change later
+        String age = ageText.getText();
         String email = emailText.getText();
         
         Connection connection = null;
@@ -61,11 +61,62 @@ public class FXMLDocumentController implements Initializable {
                          "VALUES (?, ?, ?, ?)";
             stmt = connection.prepareStatement(sql);
             
-            // Set the values for the statement
             stmt.setString(1, firstName);
             stmt.setString(2, lastName);
             stmt.setString(3, age);
             stmt.setString(4, email);
+            stmt.executeUpdate();
+            
+            stmt.close();
+            connection.close();
+            
+            Stage currentStage = (Stage) firstNameText.getScene().getWindow();
+            currentStage.close();
+            MasterDetail masterDetail = new MasterDetail();
+            masterDetail.start(new Stage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try{
+               if(stmt!=null)
+                  stmt.close();
+            } catch(SQLException se2) {
+            } try {
+               if(connection!=null)
+                  connection.close();
+            } catch(SQLException se){
+               se.printStackTrace();
+            }
+         }
+        
+    }
+    
+    
+    @FXML
+    private void handleUpdateButtonAction(ActionEvent event) {
+        
+        Employee selectedEmployee = tableView.getSelectionModel().getSelectedItem();
+
+        int employeeid = selectedEmployee.getId();
+        String firstName = firstNameText.getText();
+        String lastName = lastNameText.getText();
+        String age = ageText.getText(); 
+        String email = emailText.getText();
+        
+        Connection connection = null;
+        PreparedStatement stmt = null;
+        
+        try {
+            connection = connect.ConnectToDatabase();
+            
+            String sql = "UPDATE employees SET first_name = ?, last_name = ?, age = ? ,email = ? WHERE id = ?";
+            stmt = connection.prepareStatement(sql);
+            
+            stmt.setString(1, firstName);
+            stmt.setString(2, lastName);
+            stmt.setString(3, age);
+            stmt.setString(4, email);
+            stmt.setInt(5, employeeid);
             stmt.executeUpdate();
             
             stmt.close();
@@ -97,6 +148,9 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void handleDeleteButtonAction(ActionEvent event) {
         
+        Employee selectedEmployee = tableView.getSelectionModel().getSelectedItem();
+
+        int employeeid = selectedEmployee.getId();
         String firstName = firstNameText.getText();
         String lastName = lastNameText.getText();
         String age = ageText.getText(); 
@@ -107,11 +161,10 @@ public class FXMLDocumentController implements Initializable {
         
         try {
             connection = connect.ConnectToDatabase();
-            
-            String sql = "DELETE FROM employees WHERE email= ?";
+            String sql = "DELETE FROM employees WHERE id = ?";
             stmt = connection.prepareStatement(sql);
             
-            stmt.setString(1, email);
+            stmt.setInt(1, employeeid);
             stmt.executeUpdate();
             
             stmt.close();
@@ -142,7 +195,6 @@ public class FXMLDocumentController implements Initializable {
     private void TableMouseClickAction(MouseEvent event) {
         int index = tableView.getSelectionModel().getSelectedIndex();
         Employee selectedEmployee = tableView.getSelectionModel().getSelectedItem();
-        System.out.println("Selected employee: " + selectedEmployee);
         tableView.getSelectionModel().select(index);
         firstNameText.setText(selectedEmployee.getFirst_name());
         lastNameText.setText(selectedEmployee.getLast_name());
