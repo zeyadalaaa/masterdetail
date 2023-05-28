d19cc084f13c9048ca8526815570eb118e0612c3
zeyadalaaa <zeyad.alaaeldin2021@gmail.com>
2023-05-28 09:32:39 +0300

    Add employee and Department

---
+++ src/masterdetail/Employee.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package masterdetail;
+
+/**
+ *
+ * @author Zeyad.Alaa
+ */
+public class Employee {
+    private int id;
+    private String first_name;
+    private String last_name;
+    private int age;
+    private String email;
+    private Department department;
+
+    public Employee(int id, String first_name, String last_name, int age, String email, Department department) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.age = age;
+        this.email = email;
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+    
+}
